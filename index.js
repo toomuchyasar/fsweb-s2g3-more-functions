b311f35df5dfbe6c715b6ecbb60b7c3738822280
Yaşar Muslu <yasarmuslu93@outlook.com>
2023-03-29 18:39:47 +0300

    ez

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-// Aşağıdaki görevleri tamamlayın
-
-/*
-  GÖREV 1
-  - Input:  Bir dosya yolunu (path) parametre olarak alacak bir fonksiyon oluştur.
-  - Output: Yazacağın fonksiyon, verilen bir dosya adresideki(path) dosyanın adını vermeli.
-  * İpucu:  Dosya adresinin / işaretleri ile bölümlendiğine dikkat et.
-
-  örnek input:  "C:/Users/johnsmith/Music/Beethoven_5.mp3"
-  örnek output: "Beethoven_5.mp3"
-
-  örnek input:  "Beethoven_5.mp3"
-  örnek output: "Beethoven_5.mp3"
-
-  örnek input:  ""
-  örnek output: ""
-*/
-
-function dosyaAdiniBul(/* kodlar buraya */) {
-  // kodlar buraya
+function dosyaAdiniBul(path) {
+  const path1 = path.substring(path.lastIndexOf("/")+1)
+  return path1;
 }
 
-/*
-  GÖREV 2
-  - Input:  Bir sayı arrayini parametre olarak alacak bir fonksiyon oluştur.
-  - Output: Yazacağın fonksiyon input olarak aldığı sayı arrayinin aritmetik ortalamasını dönmeli.
 
-  örnek input:  []
-  örnek output: null
 
-  örnek input:  [4]
-  örnek output: 4
-  
-  örnek input:  [50, -26, 153, 7]
-  örnek output: 46
-
-  örnek input:  [109, 216, 288, 143, 71, 185, -278, 194, 5]
-  örnek output: 104
-*/
-
-function ortalamaBul(/* kodlar buraya */) {
-  // kodlar buraya
+function ortalamaBul(array) {
+  let toplama = array.reduce((total,sayi) => {return total + sayi},0)
+  let average = toplama/array.length
+  if(array.length==0){
+    return null
+  }else{
+    return average;
+  }
 }
+
 
 /*
   GÖREV 3
@@ -62,10 +36,19 @@ function ortalamaBul(/* kodlar buraya */) {
   örnek output: [109, 216, 288, 143, 185, 194]
 */
 
-function ortalamadanBuyukleriBul(/* kodlar buraya */) {
-  // kodlar buraya
-}
+const ornek = [109, 216, 288, 143, 71, 185, -278, 194, 5];
 
+function ortalamadanBuyukleriBul(array,callback) {
+  const big = []
+  const sayilar = array.filter((sayi) => {return sayi >= callback(array)});
+  sayilar.map((rakam) => big.push(rakam));
+  if(array.length==0){
+    return null
+  }else{
+    return big
+  }
+}
+console.log(ortalamadanBuyukleriBul(ornek,ortalamaBul))
 /* !!!! Burdan aşağısını değiştirmeyin !!!! */
 function as() {
   console.log("Kodlar sorunsuz çalışıyor!");
